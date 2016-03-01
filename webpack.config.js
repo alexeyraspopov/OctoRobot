9bc89e26ee276f73db2c8a10020a252b829e39f1
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-03-01 19:52:01 +0200

    added default entry to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
 export const development = {};
 export const testing = {};
 export const production = {};
+import Path from 'path';
+
+const entry = {
+  app: Path.resolve('ClientApp/index.web.js'),
+};
