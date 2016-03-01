3abc2e767274426e10192f0ee145ec35510ac6ee
Alexey Raspopov <alexey.raspopov@datarobot.com>
2016-03-01 12:36:24 +0200

    cleanup

--- ClientApp/App.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react';
-import Header from 'App/Header';
-import AppStyles from 'App/AppStyles';
-
-export default class App extends Component {
-	render() {
-		return <Header className={AppStyles.header} />;
-	}
-}
--- ClientApp/Header.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Header({className}) {
-	return <h1 className={className}>Hello there!</h1>;
-}
